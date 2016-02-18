d119c47ceb1e6924208cb04b5184b9ebb0b49a97
Ryan Schuetzler <ryan@schuetzler.net>
2016-02-18 10:36:53 -0600

    Add destroy function!

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -58,7 +58,14 @@ module.exports = {
 
       res.redirect('/customer/show/' + req.param('id'));
     });
+  },
+
+  destroy: function(req, res, next) {
+    Customer.destroy(req.param('id')).exec( function() {
+      res.redirect('/customer/');
+    });
   }
+
 
 
 };
